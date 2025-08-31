3761761d374747c0332dffdaca0a709a52b14113
pratyayan <pratyayanbera2003@gmail.com>
2025-08-31 13:08:56 +0530

    updated url in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://chill-chat-server.onrender.com',
         changeOrigin: true,
       },
     },
