1e90689790f4ca5ac7224f413fd24738c3d39313
pratyayan <pratyayanbera2003@gmail.com>
2025-08-31 14:43:06 +0530

    Fix cookie settings for cross-origin authentication

--- backend/src/util/JWT.js
+++ backend/src/util/JWT.js
@@ -9,8 +9,8 @@ export const generateJWT = (userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:7*24*60*60*1000,
         httpOnly:true, //prevent XSS attacks cross-site scripting attacks
-        sameSite:"strict", //CSRF attacks cross-site request forgery attacks
-        secure: process.env.NODE_ENV !=="development"
+        sameSite:"none", //Allow cross-site cookies for production
+        secure: true //Required for sameSite: none
     });
 
     return token;
